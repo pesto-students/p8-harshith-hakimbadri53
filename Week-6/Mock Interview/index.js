dd6fa8298240db15f4936952b204448d76237882
Hakimuddin Chundawada <hakim.badri53@gmail.com>
2023-01-28 20:08:28 +0530

    added customMap as a properties to array via prototype

--- Week-6/Mock Interview/index.js
+++ Week-6/Mock Interview/index.js
@@ -1,23 +1,23 @@
-// 1. What is hoisting?
-console.log(a);
-var a;
-console.log(b);
-let b;
-// 2. difference between a normal function and arrow function?
-let obj = {
-  x: 20,
-  a: function a() {
-    console.log(this.x);
-  },
-  b: () => {
-    console.log(this.x);
-  },
-};
+// // 1. What is hoisting?
+// console.log(a);
+// var a;
+// console.log(b);
+// let b;
+// // 2. difference between a normal function and arrow function?
+// let obj = {
+//   x: 20,
+//   a: function a() {
+//     console.log(this.x);
+//   },
+//   b: () => {
+//     console.log(this.x);
+//   },
+// };
 
 //3. What are polyfills, write a polyfill for map functionality
-const customMap = (iter, customFunction) => {
+Array.prototype.customMap = function (customFunction) {
   const result = [];
-  for (el of iter) {
+  for (el of this) {
     result.push(customFunction(el));
   }
   return result;
@@ -29,7 +29,7 @@ function print(val) {
   console.log(val * 2);
 }
 
-customMap(arr, print);
+arr.customMap(print);
 // need to convert this funtion in such a way that we can access directly as a property of array, using inheritance/prototype.
 //so need to work on polyfills
 
